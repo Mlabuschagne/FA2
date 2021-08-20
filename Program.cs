69ec0247e9505a827b712055482a54d44e9a342b
Mlabuschagne <89184337+Mlabuschagne@users.noreply.github.com>
2021-08-20 17:40:21 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This is FA2");
+            Console.WriteLine("Marno Labuschagne");
+            Console.WriteLine("Icas 6383");
+        }
+    }
+}
